e005cb99863059ba862178032144a0d791b07469
researchwizpro <65458634+researchwizpro@users.noreply.github.com>
2020-05-16 15:53:59 -0700

    Update RunSetup.ps1

--- RunSetup.ps1
+++ RunSetup.ps1
@@ -2,6 +2,8 @@
 # Author: WillTee
 # Lightweight Windows 10 Pro Setup Script
 
+Disable-UAC
+
 # Get the base URI path from the ScriptToCall value
 $bstrappackage = "-bootstrapPackage"
 $helperUri = $Boxstarter['ScriptToCall']
@@ -21,8 +23,8 @@ function executeScript {
 
 #--- Setting up Windows ---
 executeScript "ChangeExecutionPolicy.ps1";
-#executeScript "RemoveDefaultApps.ps1";
-#executeScript "InstallApps.ps1";
+executeScript "RemoveDefaultApps.ps1";
+executeScript "InstallApps.ps1";
 #executeScript "SystemConfiguration.ps1";
 #executeScript "FileExplorerSettings.ps1";
 #executeScript "CommonDevTools.ps1";
