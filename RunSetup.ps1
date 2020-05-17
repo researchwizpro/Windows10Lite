6938378582b6d2d1639dfb1901465e857164fdb1
Will Terfansky <will@researchwizpro.com>
2020-05-16 17:54:46 -0700

    new

--- RunSetup.ps1
+++ RunSetup.ps1
@@ -5,7 +5,7 @@
 #Disable UAC
 New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
 
-RefreshEnv
+
 
 # Get the base URI path from the ScriptToCall value
 $bstrappackage = "-bootstrapPackage"
