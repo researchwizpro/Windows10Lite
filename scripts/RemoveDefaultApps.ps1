f60eac1a07ab2b9659080cdc5802b6ff059bf63e
Will Terfansky <will@researchwizpro.com>
2020-05-16 20:16:25 -0700

    test

--- scripts/RemoveDefaultApps.ps1
+++ scripts/RemoveDefaultApps.ps1
@@ -1,6 +1,6 @@
 # Removes All Apps That Are Pre Packaged With Windows 10 Pro (except MS Store)
 
 
-get-appxpackage *onedrive* | remove-appxpackage
+get-appxpackage *OneDrive* | remove-appxpackage
 
 Get-AppxPackage | where-object {$_.name –notlike "*windowsstore*"} | Remove-AppxPackage
