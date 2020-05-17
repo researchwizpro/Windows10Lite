79f6b6abf447a5dc817b4bf83c23df046c47558f
Will Terfansky <will@researchwizpro.com>
2020-05-17 14:11:16 -0700

    test

--- RunSetup.ps1
+++ RunSetup.ps1
@@ -38,7 +38,7 @@ $ConfirmPreference = "None"
 Stop-Process -Name "Microsoftedge"
 
 # Installs Chocolatey
-Set-ExecutionPolicy Unrestricted -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Installs AutoLogon
 choco install autologon
