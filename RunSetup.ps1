694230932f7ddc555da76dd340d7d3745429083f
Will Terfansky <will@researchwizpro.com>
2020-05-17 14:26:26 -0700

    test

--- RunSetup.ps1
+++ RunSetup.ps1
@@ -40,8 +40,8 @@ Stop-Process -Name "Microsoftedge"
 
 
 # Get the base URI path from the ScriptToCall value
-#$bstrappackage = "-bootstrapPackage"
-#$helperUri = $Boxstarter['ScriptToCall']
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
 $strpos = $helperUri.IndexOf($bstrappackage)
 $helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
 $helperUri = $helperUri.TrimStart("'", " ")
