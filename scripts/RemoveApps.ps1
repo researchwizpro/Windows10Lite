015a54d33d83fc27e36ca5bd40863cc261a36247
Will Terfansky <will@researchwizpro.com>
2020-05-20 22:10:23 -0700

    testNew

--- scripts/RemoveApps.ps1
+++ scripts/RemoveApps.ps1
@@ -1,3 +1,3 @@
-# Removes All Apps That Are Pre Packaged With Windows 10 Pro (except MS Store)
+# Removes All Apps That Are Pre Packaged With Windows 10 Pro (except MS Store, Calculator)
 
 Get-AppxPackage | where-object {$_.name –notlike "*windowsstore*"} | Remove-AppxPackage
